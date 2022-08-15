fcdd19b7598bf3f9e54035bb4cffb61160b1431e
Mathis Gertz <mathis.gertz@sinnerschrader.com>
2022-08-15 20:03:04 +0200

    :)

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
-import './style.css';
+import "./style.css";
 
-import * as _ from 'lodash';
+import * as _ from "lodash";
 
-import userPng from './assets/user.png';
-import data from './data/de.json';
-import printMe from './print';
+import userPng from "./assets/user.png";
+import data from "./data/de.json";
+import printMe from "./print";
 
 const strings = data;
 
 function main() {
   const element = document.createElement("div");
-  element.innerHTML = strings.hello;
-  element.classList.add("hello");
+  element.classList.add("wrapper");
 
+  const headline = document.createElement("h1");
+  headline.innerHTML = strings.headline;
+  element.appendChild(headline);
+
+  const textWithIcon = document.createElement("div");
   const icon = new Image();
   icon.src = userPng;
-  element.appendChild(icon);
+  textWithIcon.innerHTML = strings.iconText;
+  textWithIcon.appendChild(icon);
+  element.appendChild(textWithIcon);
 
   const button = document.createElement("button");
   button.onclick = printMe;
